c84b28909eacb391ca0fc294fe5d83e09dec897b
Lucas Luzini <lucasluzini@hotmail.com>
2021-11-03 21:00:58 -0300

    Editado por Lucas Luzini

--- script.js
+++ script.js
@@ -1,46 +1,5 @@
-let numeroAleatorio=[];
-// let arrayJogador=[];
-
-
-const gerarNumero = function (){
-    numeroAleatorio.push(Math.trunc(Math.random()*4) + 1);  
-}
-
-// const arr = function (num){
-//     arrayJogador.push(num);  
-// }
-
-const comparar = function (numeroAleatorio, numeroPressionado) {
-
-    // numeroAleatorio.forEach(item => {
-        
-    // });
-
-    // numeroAleatorio.every(function(element, index) {
-    //     // Do your thing, then:
-    //     if (numeroPressionado !== element) {
-    //         console.log("-----------");
-    //         console.log(numeroPressionado);
-    //         console.log(element);
-    //         return false
-    //     }
-    //     else return true
-    //   })
-
-    for (let i = 0; i<=numeroPressionado.length; i++){
-        if(numeroAleatorio[i] === numeroPressionado[i]){
-            return true;
-        }else{
-            gameOver();
-            break;
-        }
-    }
-    gerarNumero();
-}
-
-gerarNumero();
-comparar(numeroAleatorio, 1);
-comparar(numeroAleatorio, 2);
-comparar(numeroAleatorio, 3);
-comparar(numeroAleatorio, 1);
-
+let level = 0;
+let started = false;
+let coresBotoes = ["red", "blue", "green", "yellow"];
+let coresAleatoriasGeradas = [];
+let coresEscolhidas = [];
